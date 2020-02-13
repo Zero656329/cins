c7bff359485740c492f15388d493b5b211323ff0
Zero656329 <hwj656329@163.com>
2020-02-13 23:39:30 +0800

    社交

---
+++ src/main/java/com/tong/cins/CinsApplication.java
@@ -0,0 +1,13 @@
+package com.tong.cins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tong/cins/CinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tong.cins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
